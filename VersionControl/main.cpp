3cef53f683f2edb06a961e02eab1058bdeba0b32
liuyuxuan123 <1010134430@qq.com>
2019-05-26 16:52:35 +0800

    Initial Commit

---
+++ VersionControl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VersionControl
+//
+//  Created by 刘宇轩 on 2019/5/26.
+//  Copyright © 2019 yuxuanliu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
